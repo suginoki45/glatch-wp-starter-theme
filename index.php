5c4a49eb55dc66a35f012f775e2701a099076dfd
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-01-02 08:47:37 +0900

    index.phpを追加

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The Index Page template.
+ *
+ * @package   glatch-wp-starter
+ * @copyright Copyright (c) 2018 Glatch
+ * @license   GNU General Public License v2.0
+ * @since     drip 1.0.0
+ */
+
+get_header();
