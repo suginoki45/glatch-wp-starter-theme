29128e74fadac745de55091ba2924086c769f7e1
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-01-14 11:29:00 +0900

    footer.phpを追加

---
+++ footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The Footer template.
+ *
+ * @package   _yourthemename
+ * @copyright Copyright (c) 2018 Glatch
+ * @license   GNU General Public License v2.0
+ * @since     drip 1.0.0
+ */
+
+?>
+
+<footer class="p-footer">
+	<?php if ( is_active_sidebar( 'wp-widgets-footer' ) ) { ?>
+		<section class="fd-section">
+			<?php dynamic_sidebar( 'wp-widgets-footer' ); ?>
+		</section>
+	<?php } ?>
+	<p class="c-copyright"><small>©
+	<?php
+		echo esc_html( date( 'Y' ) );
+		bloginfo( 'name' );
+	?>
+	</small></p>
+	<a class="c-pagetop" href="#pagetop">Page Top</a>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
