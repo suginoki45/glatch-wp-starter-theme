df54bceba2e7374441e73df4c29ec364e48f16d1
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-01-14 11:29:16 +0900

    サイドバーを追加

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The Sidebar template.
+ *
+ * @package   _yourthemename
+ * @copyright Copyright (c) 2018 Glatch
+ * @license   GNU General Public License v2.0
+ * @since     drip 1.0.0
+ */
+
+?>
+
+<aside class="fd-section">
+	<?php dynamic_sidebar( 'wp-widgets-sidebar' ); ?>
+</aside>
