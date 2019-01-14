f399685eef75f58e2ae7d6e0efc03f126d01c77c
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-01-14 11:33:10 +0900

    index.phpにメインとフッターを追加

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
  * @package   _yourthemename
  * @copyright Copyright (c) 2018 Glatch
  * @license   GNU General Public License v2.0
- * @since     drip 1.0.0
+ * @since     _yourthemename 1.0.0
  */
 
 get_header();
+
+?>
+
+<main></main>
+
+<?php
+get_footer();
