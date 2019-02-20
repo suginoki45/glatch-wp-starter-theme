f1a981c45a93261860fc61584d45993ca8d673df
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-02-20 14:53:32 +0900

    ハンバーガーメニューを追加

--- tmp/menu-primary.php
+++ tmp/menu-primary.php
@@ -1,11 +1,18 @@
+<div class="c-toggle">
+	<button type="button" class="c-hamburger" id="js-hamburger">
+		<span></span>
+		<span></span>
+		<span></span>
+	</button>
+</div>
 <nav class="p-global-navi">
 	<?php
 		wp_nav_menu(
 			array(
 				'theme_location' => 'global_navi',
 				'container'      => false,
-				'menu_class'     => 'fd-section c-nav-menu',
-				'items_wrap'     => '<ul class="%2$s">%3$s</ul>',
+				'menu_class'     => 'c-nav-menu',
+				'menu_id'        => 'js-nav-menu',
 			)
 		);
 		?>
