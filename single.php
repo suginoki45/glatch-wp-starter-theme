61ccd764170173c24b6c328af40adb8d645d272e
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-03-20 17:45:21 +0900

    個別記事ページ関連のテンプレートを追加

--- single.php
+++ single.php
@@ -1,2 +1,27 @@
 <?php
+/**
+ * The Single Page template.
+ *
+ * @package   _yourthemename
+ * @copyright Copyright (c) 2018 Glatch
+ * @license   GNU General Public License v2.0
+ * @since     _yourthemename 1.0.0
+ */
+
+$category = get_the_category();
+
 get_header();
+?>
+<main class="c-main">
+<?php
+while ( have_posts() ) {
+	the_post();
+	get_template_part( 'tmp/content', 'single' );
+}
+?>
+</main>
+
+<?php get_template_part( 'tmp/breadcrumbs' ); ?>
+
+<?php
+get_footer();
---
+++ tmp/content-single.php
@@ -0,0 +1,42 @@
+<?php
+$category = get_the_category();
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'c-entry' ); ?>>
+	<div class="inner">
+		<ul class="c-meta">
+			<li class="c-meta__item is-category">
+				<a href="<?php echo esc_html( get_category_link( $category[0]->term_id ) ); ?>"><?php echo esc_html( $category[0]->category_nicename ); ?></a>
+			</li>
+			<li class="c-meta__item is-date">
+				<?php the_time( 'Y.m.d' ); ?>
+			</li>
+		</ul>
+		<?php the_title( '<h1 class="c-entry__heading">', '</h1>' ); ?>
+		<?php get_template_part( 'tmp/share-buttons' ); ?>
+		<?php
+		if ( has_post_thumbnail() ) {
+			?>
+				<div class="c-entry__thumbnail">
+					<?php echo get_the_post_thumbnail(); ?>
+				</div>
+			<?php
+		} else {
+			?>
+			<div class="c-entry__thumbnail">
+				<img src="<?php get_url_with_update_query( '/dist/images/img_degault-thumbnail.png' ); ?>" alt="デフォルトのサムネイル">
+			</div>
+			<?php
+		}
+		?>
+		<div class="c-entry__content">
+			<?php the_content(); ?>
+		</div>
+		<footer class="c-entry__footer">
+			<div class="c-entry__share-buttons">
+				<p class="c-entry__share-buttons__label">share:</p>
+				<?php get_template_part( 'tmp/share-buttons' ); ?>
+			</div>
+		</footer>
+	</div>
+</article>
