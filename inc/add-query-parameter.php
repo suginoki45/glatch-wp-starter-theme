75f7149374769671a8fb17bfb984a99fdde0cf23
Yuki Sugitani <yuki.sugitani@glatchdesign.com>
2019-03-20 18:02:50 +0900

    phpファイルを整形

--- inc/add-query-parameter.php
+++ inc/add-query-parameter.php
@@ -2,7 +2,7 @@
 
 function get_url_with_update_query( $file_name ) {
 	$file_directory = get_template_directory() . $file_name;
-	$file_url = get_theme_file_uri( $file_name );
+	$file_url       = get_theme_file_uri( $file_name );
 
 	if ( file_exists( $file_directory ) ) {
 		echo esc_url( add_query_arg( 'ver', date( 'ymdHis', filemtime( $file_directory ) ), $file_url ) );
