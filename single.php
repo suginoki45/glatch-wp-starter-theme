6edd8e3f026f8f03a3445c08696b6f409aa5519e
suginoki45 <yuki.sugitani@glatchdesign.com>
2020-09-05 11:04:11 +0900

    リファクタリング

--- single.php
+++ single.php
@@ -1,23 +1,20 @@
 <?php
 /**
- * The Single Page template.
+ * The single page template
  *
- * @package   _yourthemename
- * @copyright Copyright (c) 2018 Glatch
- * @license   GNU General Public License v2.0
- * @since     _yourthemename 1.0.0
+ * @package _yourthemename
+ * @author  Glatch
  */
 
 get_header();
 ?>
-<main class="c-main">
+
 <?php
-while ( have_posts() ) {
+while ( have_posts() ) :
 	the_post();
 	get_template_part( 'tmp/content', 'single' );
-}
+endwhile;
 ?>
-</main>
 
 <?php get_template_part( 'tmp/breadcrumbs' ); ?>
 
